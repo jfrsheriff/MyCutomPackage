43ecf576fdc3c8ca4b3376bc9957c3b7e173f3e2
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2024-05-13 13:38:53 +0530

    Initial Commit

---
+++ Sources/MyCutomPackage/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jaffer Sheriff U on 13/05/24.
+//
+
+import Foundation
+
+public struct Calculator{
+    public static func add(num1: Int, num2: Int) -> Int{
+        print("Adding Of \(num1) and \(num2)")
+        return num1 + num2
+    }
+}
