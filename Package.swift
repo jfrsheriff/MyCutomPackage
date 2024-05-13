4e6fbb18c83a8adb6492785c40857daf30b5df5d
Jaffer Sheriff <jaffersheriff.u@zohocorp.com>
2024-05-13 17:01:23 +0530

    Changes

--- Package.swift
+++ Package.swift
@@ -12,12 +12,9 @@ let package = Package(
             targets: ["MyCutomPackage"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyCutomPackage"),
-        .testTarget(
-            name: "MyCutomPackageTests",
-            dependencies: ["MyCutomPackage"]),
+        .binaryTarget(
+            name: "MyCutomPackage",
+            path: "./Sources/CustomFrameworkProject.xcframework"
+        ),
     ]
 )
---
+++ Sources/CustomFrameworkProject.xcframework/ios-arm64_x86_64-simulator/CustomFrameworkProject.framework/Headers/CustomFrameworkProject.h
@@ -0,0 +1,18 @@
+//
+//  CustomFrameworkProject.h
+//  CustomFrameworkProject
+//
+//  Created by Jaffer Sheriff U on 13/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFrameworkProject.
+FOUNDATION_EXPORT double CustomFrameworkProjectVersionNumber;
+
+//! Project version string for CustomFrameworkProject.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFrameworkProject/PublicHeader.h>
+
+
